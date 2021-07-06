409fdf726b12a56501575024db2da016a0e4d322
“SC-zzz” <shulichen1996@gmail.com>
2021-07-06 01:00:25 -0700

    upload Chapter 03 Demo.html; modify Chapter 02 Demo.html

---
+++ js/draw_logistic.js
@@ -0,0 +1,264 @@
+//var graphcolor = 'RGB(52, 94, 101)'
+//var taylorcolor = 'RGB(70, 170, 185)'
+
+
+var canvasdiv = document.getElementById('canvasdiv');
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+// plotting range: -1<= x <=6, -1<= y <= 6
+// some plotting parameters
+var grid = {
+	gridcolor: "gray",
+	gridwidth: "1",
+	axiscolor: "black",
+	axiswidth: "2",
+	labelcolor: "#888",
+	labelsize: "18",
+	plotcolor: "dodgerblue",
+	plotwidth: "4",
+	stationarycolor: "cyan",
+	stationarywidth: "3",
+	vectorcolor: "#8080ff",
+	vectorwidth: "2",
+	aspect: 8/7, 
+	paddingleft: 20,	
+	paddingright: 20,
+	wd: Math.max(500, Math.min(canvasdiv.offsetWidth, 7/8.5 * window.innerHeight))	  // get width according to window size
+}
+	
+    
+var ht = (grid.wd-grid.paddingleft-grid.paddingright)*grid.aspect;
+canvas.width = grid.wd;
+canvas.height = ht;	  
+
+
+
+// convert from graph coordinates to canvas pixels
+var scaling_factor_x = (canvas.width-grid.paddingleft-grid.paddingright)/7;
+var scaling_factor_y = canvas.height/8;
+
+
+function y_px(y_coord){
+  return 7*canvas.height/8 - scaling_factor_y * y_coord;
+}
+
+function x_px(x_coord){
+  return grid.paddingleft + canvas.width/7 + scaling_factor_x * x_coord;
+}
+
+// draw underlying grids
+function drawGrid() {
+    for (let i =0; i<6; i++) {           
+            ctx.beginPath();
+	        ctx.moveTo(x_px(i), y_px(-1));
+	        ctx.lineTo(x_px(i), y_px(6));
+	        ctx.strokeStyle = grid.gridcolor;
+	        ctx.lineWidth = grid.gridwidth;
+	        ctx.stroke();
+    }
+    for (let j =0; j<6; j++) {          
+            ctx.beginPath();
+	        ctx.moveTo(x_px(-1), y_px(j));
+	        ctx.lineTo(x_px(6), y_px(j));
+	        ctx.strokeStyle = grid.gridcolor;
+	        ctx.lineWidth = grid.gridwidth;
+	        ctx.stroke();
+    }    
+} 
+
+function drawAxis() {
+    // x-axis
+    ctx.beginPath();
+	ctx.moveTo(x_px(-1), y_px(0));
+	ctx.lineTo(x_px(6), y_px(0));
+	ctx.strokeStyle = grid.axiscolor;
+	ctx.lineWidth = grid.axiswidth;
+	ctx.stroke();
+	ctx.font = "18px Arial";
+	ctx.textAlign = "right";
+	ctx.textBaseline = "top";
+	ctx.fillText("t", x_px(6),y_px(-0.05));
+	for (let i = 1; i<6; i=i+2) { 
+	    ctx.font = "16px Arial";
+	    ctx.textAlign = "center";
+	    ctx.textBaseline = "top";
+	    ctx.fillText(i, x_px(i),y_px(-0.05));
+	}
+	
+	// y-axis
+    ctx.beginPath();
+	ctx.moveTo(x_px(0), y_px(-1));
+	ctx.lineTo(x_px(0), y_px(6));
+	ctx.strokeStyle = grid.axiscolor;
+	ctx.lineWidth = grid.axiswidth;
+	ctx.stroke();
+	ctx.font = "18px Arial";
+	ctx.textAlign = "right";
+	ctx.textBaseline = "top";
+	ctx.fillText("y", x_px(-0.05),y_px(6));
+	for (let j = 1; j<6; j=j+2) { 
+	    ctx.font = "16px Arial";
+	    ctx.textAlign = "right";
+	    ctx.textBaseline = "middle";
+	    ctx.fillText(j, x_px(-0.05),y_px(j));
+	}
+}
+
+
+// draw arrows on canvas
+function canvas_arrow(fromx, fromy, tox, toy) {
+  var headlen = 5; // length of head in pixels
+  var dx = tox - fromx;
+  var dy = toy -  fromy;
+  var angle = Math.atan2(dy, dx);
+  ctx.beginPath();
+  ctx.moveTo(fromx, fromy);
+  ctx.lineTo(tox, toy);
+  ctx.strokeStyle = grid.vectorcolor;
+  ctx.lineWidth = grid.vectorwidth;
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(tox, toy);  
+  ctx.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
+  ctx.strokeStyle = grid.vectorcolor;
+  ctx.lineWidth = grid.vectorwidth;
+  ctx.stroke();
+  
+  ctx.beginPath();  
+  ctx.moveTo(tox, toy);
+  ctx.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+  ctx.strokeStyle = grid.vectorcolor;
+  ctx.lineWidth = grid.vectorwidth;
+  ctx.stroke();
+}
+
+
+// draw direction field or slope field. 1= unit direction field, 2 = slope field
+function draw_direction_field(option, r, K, Y0){
+    var xDF = math.range(-1, 6, 0.4).toArray();
+    var yDF = math.range(-1, 6, 0.4).toArray();
+    var scaling = 0.3;
+    var dyDF = yDF.map(function (y) {
+         return math.evaluate('r*y*(1-y/K)', {y:y, r:r, K:K, Y0:Y0})
+           });
+    var lengths = dyDF.map(function (y) {
+         return math.evaluate('sqrt(1+y^2)', {y:y})
+           }); 
+    var vxs_normalized = dyDF.map(function (y) {
+         return scaling*math.evaluate('1/sqrt(1+y^2)', {y:y})
+           });  
+    var vys_normalized = dyDF.map(function (y) {
+         return scaling*math.evaluate('y/sqrt(1+y^2)', {y:y})});       
+    if (option == 1) {
+        for (let i =0; i < xDF.length; i++) {
+            for (let j =0; j < yDF.length; j++) {
+                //canvas_arrow(x_px(xDF[i]), y_px(yDF[j]), x_px(vxs_normalized[i]), y_px(vys_normalized[j]));
+                canvas_arrow(x_px(xDF[i]), y_px(yDF[j]), x_px(xDF[i]+vxs_normalized[j]), y_px(yDF[j]+vys_normalized[j]));
+            }
+        }
+    }
+    if (option == 2) {
+        for (let i =0; i < xDF.length; i++) {
+            for (let j =0; j < yDF.length; j++) {
+                    ctx.beginPath();
+	                ctx.moveTo(x_px(xDF[i]), y_px(yDF[j]));
+	                ctx.lineTo(x_px(xDF[i]+vxs_normalized[j]), y_px(yDF[j]+vys_normalized[j]));
+	                ctx.strokeStyle = grid.vectorcolor;
+	                ctx.lineWidth = grid.vectorwidth;
+	                ctx.stroke();
+            }
+        }
+    }
+} 
+
+
+
+
+
+function new_function() { 
+    ctx.clearRect(0, 0, canvasdiv.offsetWidth, canvasdiv.offsetHeight);
+    drawGrid();
+    drawAxis();
+    var r = parseFloat(document.getElementById("r").value);  
+    var K = parseFloat(document.getElementById("K").value);
+    var Y0 = parseFloat(document.getElementById("Y0").value); 
+    ctx.font = "20px Arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "top";
+    ctx.fillText("The logistic model", x_px(3),y_px(7));
+    let str = "(with r= " + r + ", K=" + K + ", y_0 =" + Y0 +")";
+    ctx.fillText(str, x_px(3),y_px(6.5));
+    
+    // draw direction field
+    if (document.getElementById("DF").checked) {
+        draw_direction_field(option=1, r, K, Y0);
+    }
+    else if (document.getElementById("slope").checked) {
+        draw_direction_field(option=2, r, K, Y0);
+    }
+    
+    // draw stationary solutions
+    if (document.getElementById("showStationary").checked) {
+        ctx.beginPath();
+	    ctx.moveTo(x_px(-1), y_px(0));
+	    ctx.lineTo(x_px(6), y_px(0));
+        ctx.strokeStyle = "green";
+	    ctx.lineWidth = grid.stationarywidth;
+	    ctx.stroke();
+	    
+	    ctx.beginPath();
+	    ctx.moveTo(x_px(-1), y_px(K));
+	    ctx.lineTo(x_px(6), y_px(K));
+        ctx.strokeStyle = "red";
+	    ctx.lineWidth = grid.stationarywidth;
+	    ctx.stroke();
+    }
+    
+    
+    if (Y0 > K) {
+    let t_break = Math.log(-(K-Y0)/Y0)/r;
+    let t_start = math.max(t_break+0.01, -1);
+    var xArray = math.range(t_start, 6, 0.01).toArray();
+    var yArray = xArray.map(function (x) {
+         return math.evaluate('(K * Y0)/(Y0 +(K-Y0)*exp(-r*x))', {x: x, r:r, K:K, Y0:Y0})
+           });
+ } 
+  else{
+    var xArray = math.range(-1, 6, 0.01).toArray();
+    if (Y0 == 0) {
+      var yArray = new Array(xArray.length);
+      for (let j=0; j < xArray.length; ++j) yArray[j] = 0;
+     } 
+    else {
+     var yArray = xArray.map(function (x) {
+         return math.evaluate('(K * Y0)/(Y0 +(K-Y0)*exp(-r*x))', {x: x, r:r, K:K, Y0:Y0})
+           });
+     }
+ }
+  for (let i=0; i < xArray.length-1; i++) {
+    ctx.beginPath();
+	ctx.moveTo(x_px(xArray[i]), y_px(yArray[i]));
+	ctx.lineTo(x_px(xArray[i+1]), y_px(yArray[i+1]));
+	ctx.strokeStyle = grid.plotcolor;
+	ctx.lineWidth = grid.plotwidth;
+	ctx.stroke();
+  }
+}
+ 
+new_function();
+
+document.getElementById('r').oninput = function (event) {
+  event.preventDefault();
+    new_function();
+  }
+document.getElementById('K').oninput = function (event) {
+  event.preventDefault();
+    new_function();
+  }
+document.getElementById('Y0').oninput = function (event) {
+  event.preventDefault();
+    new_function();
+  } 
+  
+
