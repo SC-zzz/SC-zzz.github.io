1e0f29bc97434da296c835ef104f24d832ef983b
“SC-zzz” <shulichen1996@gmail.com>
2021-07-07 22:55:22 -0700

    upload files

--- js/auxiliary.js
+++ js/auxiliary.js
@@ -75,3 +75,84 @@ function drawGrid(ctx, startx, endx, numstepx, starty, endy, numstepy, color, wi
     }    
 } 
  */
+ 
+ 
+
+/**
+ * Solve a system of first-order ODE y' = f(t,y) numerically using Euler's method.
+ * @param {Float} t_0: The starting time.
+ * @param {Float} t_1: The ending time.
+ * @param {Float} h: The stepsize.
+ * @param {function} f: The function.  
+ * @param {Float} or {Array} y_0: The initial value. Float for 1d case, Array for higher dim
+ * @returns {Array} [ts, ys]: ts is an Array storing the time steps
+                              ys is an Array storing the function values
+ */
+function ode_Euler(t_0, t_1, h, f, y_0) {
+	const N = Math.floor((t_1 - t_0)/h) + 1;
+	var ts = Array.from(Array(N), (_, k) => k * h + t_0);
+	var ys = Array(N).fill(y_0);  //initialize the array for the results
+
+	for (let i = 0; i < N-1; i++) {
+	  ys[i + 1] =  math.add(ys[i], math.multiply(f(ts[i], ys[i]), h));
+	}
+	return [ts, ys];
+}
+
+
+/**
+ * Solve a system of first-order ODE y' = f(t,y) numerically using the midpoint method.
+ * @param {Float} t_0: The starting time.
+ * @param {Float} t_1: The ending time.
+ * @param {Float} h: The stepsize.
+ * @param {function} f: The function.  
+ * @param {Float} or {Array} y_0: The initial value. Float for 1d case, Array for higher dim
+ * @returns {Array} [ts, ys]: ts is an Array storing the time steps
+                              ys is an Array storing the function values
+ */
+ 
+function ode_midpoint(t_0, t_1, h, f, y_0) {
+	const N = Math.floor((t_1 - t_0)/h) + 1;
+	var ts = Array.from(Array(N), (_, k) => k * h + t_0);
+	var ys = Array(N).fill(y_0);  //initialize the array for the results
+
+	for (let i = 0; i < N-1; i++) {
+	  const k1 = f(ts[i], ys[i]);
+	  const s = math.add(ys[i], math.multiply(k1, h/2));
+	  const k2 = f(ts[i] + h/2, s);
+	  ys[i + 1] =  math.add(ys[i], math.multiply(k2, h));
+	}
+	return [ts, ys];
+}
+
+
+/**
+ * Solve a system of first-order ODE y' = f(t,y) numerically using the Runge Kutta 4th 
+ * order method.
+ * @param {Float} t_0: The starting time.
+ * @param {Float} t_1: The ending time.
+ * @param {Float} h: The stepsize.
+ * @param {function} f: The function.  
+ * @param {Float} or {Array} y_0: The initial value. Float for 1d case, Array for higher dim
+ * @returns {Array} [ts, ys]: ts is an Array storing the time steps
+                              ys is an Array storing the function values
+ */
+function ode_RK4(t_0, t_1, h, f, y_0) {
+	const N = Math.floor((t_1 - t_0)/h) + 1;
+	var ts = Array.from(Array(N), (_, k) => k * h + t_0);
+	var ys = Array(N).fill(y_0);  //initialize the array for the results
+
+	for (let i = 0; i < N-1; i++) {
+	  const k1 = f(ts[i], ys[i]);
+	  const s1 = math.add(ys[i], math.multiply(k1, h/2));
+	  const k2 = f(ts[i] + h/2, s1);
+	  const s2 = math.add(ys[i], math.multiply(k2, h/2));
+	  const k3 = f(ts[i] + h/2, s2);
+	  const s3 = math.add(ys[i], math.multiply(k2, h));	
+      const k4 = f(ts[i] + h, s3); // f(t + h, y_n + k3*h)	
+      const k_new =  math.multiply(math.add(math.add(math.add(math.multiply(k1, 1/2), k2), 
+          k3), math.multiply(k4, 1/2)), 1/3); // k1/6 + k2/3 + k3/3 + k4/6
+	  ys[i + 1] =  math.add(ys[i], math.multiply(k_new, h));
+	}
+	return [ts, ys];
+}
