93e773fa7b019aaba3635cc2f141cc226c195a75
“SC-zzz” <shulichen1996@gmail.com>
2021-08-11 22:24:59 -0700

    modify files

--- js/Bessel_approximation.js
+++ js/Bessel_approximation.js
@@ -15,6 +15,87 @@ function Jn_truncate(n, m, t)  {
   return sum;
 }
 
+function setup_Jn() { 
+  var n = parseFloat(document.getElementById("n").value);  
+  var m = parseFloat(document.getElementById("m").value);  
+  
+  var left_lim = 0;
+  var right_lim = 25;
+
+  
+  let h = 0.01;
+  let N = Math.ceil((right_lim-left_lim)/h +1);
+  let tArray = Array.from(Array(N), (_, k) => left_lim + k * h);       
+
+  let JArray = tArray.map(function (t) {return Jn_truncate(n, 42, t)});   
+  let approxArray = tArray.map(function (t) {return Jn_truncate(n, m, t)});
+
+
+  let tArray_2 = Array.from(Array(N-2), (_, k) => left_lim + k * h + 2*h); 
+  let testArray;         
+  if (n % 4 == 0) {
+    testArray = tArray_2.map(function (t) {return math.sqrt(2/(Math.PI*t))*Math.cos(t-Math.PI/4)});       
+  }
+  else if (n % 4 == 2){
+    testArray = tArray_2.map(function (t) {return -math.sqrt(2/(Math.PI*t))*Math.cos(t-Math.PI/4)});
+  } 
+  else if (n % 4 == 1){
+    testArray = tArray_2.map(function (t) {return math.sqrt(2/(Math.PI*t))*Math.sin(t-Math.PI/4)});
+  } 
+  else {
+    testArray = tArray_2.map(function (t) {return -math.sqrt(2/(Math.PI*t))*Math.sin(t-Math.PI/4)});
+  } 
+    
+
+  // Define Data
+    var data = [
+    	{
+		x: tArray,
+		y: JArray,
+		mode:"lines",
+		name: 'J_n',
+		line: {color: 'dodgerblue', width: 3}
+		},
+		{
+		x: tArray_2,
+		y: testArray,
+		mode:"lines",
+		name: 'Asymptotic behavior',
+		line: {color: 'red', width: 2}
+		},
+		{
+		x: tArray,
+		y: approxArray,
+		mode:"lines",
+		name: 'J_{' + n + ','+(2*m + n) +'}',
+		line: {color: 'darkorange', width: 3}
+		}
+	   ];
+ 
+	// Define Layout
+  var layout = {
+	   xaxis: {range: [left_lim, right_lim], title: "t"},
+	   yaxis: {range: [-1, 5], title: "y"},  
+	   title: "Approximation of J_" + n,
+	   showlegend: true,
+	   legend: {"orientation": "h", yanchor: 'top', y:-0.2},
+// 	   margin: {
+//        l: 30,
+//        r: 30,
+//        b: 100,
+//        t: 100,
+//        pad: 4
+//      },
+	};
+
+    // Display using Plotly
+    Plotly.newPlot("JnPlot", data, layout);
+}
+
+
+
+
+
 
 function new_Jn() { 
   var n = parseFloat(document.getElementById("n").value);  
@@ -48,71 +129,21 @@ function new_Jn() {
   } 
     
 
-  if (document.getElementById("showAs").checked) {
-  // Define Data
-    var data = [
-    	{
-		x: tArray,
-		y: JArray,
-		mode:"lines",
-		name: 'J_n',
-		line: {color: 'dodgerblue', width: 3}
-		},
-		{
-		x: tArray_2,
-		y: testArray,
-		mode:"lines",
-		name: 'Asymptotic behavior',
-		line: {color: 'red', width: 2}
-		},
-		{
-		x: tArray,
-		y: approxArray,
-		mode:"lines",
-		name: 'J_{' + n + ','+(2*m + n) +'}',
-		line: {color: 'darkorange', width: 3}
-		}
-	   ];
-  }
-  else {
-    var data = [
-        {
-		x: tArray,
-		y: JArray,
-		mode:"lines",
-		name: 'J_n',
-		line: {color: 'dodgerblue', width: 3}
-		},
-		{
-		x: tArray,
-		y: approxArray,
-		mode:"lines",
-		name: 'J_{' + n + ','+(2*m + n) +'}',
-		line: {color: 'darkorange', width: 3}
-		}
-		];
-  }
-	// Define Layout
-  var layout = {
-	   xaxis: {range: [left_lim, right_lim], title: "t"},
-	   yaxis: {range: [-1, 5], title: "y"},  
-	   title: "Approximation of J_" + n,
-	   showlegend: true,
-	   legend: {"orientation": "h", yanchor: 'top', y:-0.2},
-// 	   margin: {
-//        l: 30,
-//        r: 30,
-//        b: 100,
-//        t: 100,
-//        pad: 4
-//      },
-	};
+  // update
+  var data_0 = 	{x:[tArray], y:[JArray]};
+  var data_1 = 	{x:[tArray_2], y:[testArray]};
+  var data_2 =  {x:[tArray], y:[approxArray]};
 
-    // Display using Plotly
-    Plotly.newPlot("JnPlot", data, layout);
+
+  // Display using Plotly
+  Plotly.restyle("JnPlot", data_0, 0);
+  Plotly.restyle("JnPlot", data_1, 1);
+  Plotly.restyle("JnPlot", data_2, 2);
+
+
 }
 
-new_Jn();
+setup_Jn();
 
 
 
